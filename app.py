6d174a647dc9b33f6355a018b60c425e4ea69ee2
nowicki-yari <yari.nowicki@outlook.com>
2021-10-23 09:41:22 +0200

    first demo

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+pd.options.plotting.backend = "plotly"
+
+df = pd.read_csv('weather.csv')
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -11,20 +16,25 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World'),
+    html.H2('Weather App prototype'),
     dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
+        id='dropdown-time',
+        options=[{'label': i, 'value': i} for i in ['9am', '3pm']],
+        value='9am'
     ),
-    html.Div(id='display-value')
+    dcc.Graph(id='display-value')
 ])
 
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-                [dash.dependencies.Input('dropdown', 'value')])
+@app.callback(dash.dependencies.Output('display-value', 'figure'),
+                [dash.dependencies.Input('dropdown-time', 'value')])
 def display_value(value):
-    return 'You have selected "{}"'.format(value)
+    df_filtered = df.filter(regex='{}$'.format(value), axis=1)
+    df_filtered = df_filtered.drop(['WindDir{}'.format(value)], axis=1)
+    print(df_filtered)
+    fig = px.line(df_filtered)
+    fig.update_layout()
+    return fig
 
 
 if __name__ == '__main__':
